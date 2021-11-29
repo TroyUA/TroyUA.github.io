d63ad7eac25071276c7d6dadbe0dc198ae9f8a8d
TroyUA <78907806+TroyUA@users.noreply.github.com>
2021-11-29 17:24:38 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
+const initLanguageList = [
+  {
+    flag: '/src/img/language/gbr.jpg',
+    language: 'English',
+    short: 'EN',
+    checked: true,
+  },
+  {
+    flag: '/src/img/language/frn.jpg',
+    language: 'Francias',
+    short: 'FR',
+    checked: false,
+  },
+  {
+    flag: '/src/img/language/rus.jpg',
+    language: 'Русский',
+    short: 'RU',
+    checked: false,
+  },
+
+  {
+    flag: '/src/img/language/ukr.svg',
+    language: 'Українська',
+    short: 'UA',
+    checked: false,
+  },
+]
+
+let languageList = initLanguageList
+
 const select = () => {
   const selectHeader = document.querySelectorAll('.select__header')
   const selectItem = document.querySelectorAll('.select__item')
